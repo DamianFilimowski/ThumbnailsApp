c07eeb2fca4b6b31458660db631bb035a9a0bcde
DamianFilimowski <damianFilimowski@gmail.com>
2023-09-24 20:36:01 +0200

    auto creation of thumbnail sizes and preset plans

--- api/apps.py
+++ api/apps.py
@@ -1,6 +1,21 @@
 from django.apps import AppConfig
+from django.db.utils import IntegrityError
 
 
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
+
+    def ready(self):
+        try:
+            from api.models import ThumbnailSize, Plan
+            thumbnail_200, created_200 = ThumbnailSize.objects.get_or_create(height=400)
+            thumbnail_400, created_400 = ThumbnailSize.objects.get_or_create(height=200)
+            basic, created_basic = Plan.objects.get_or_create(name='Basic')
+            basic.sizes.add(thumbnail_200)
+            premium, created_premium = Plan.objects.get_or_create(name='Premium', original=True)
+            premium.sizes.add(thumbnail_200, thumbnail_400)
+            enterprise, created_enterprise = Plan.objects.get_or_create(name='Enterprise', original=True, exp_link=True)
+            enterprise.sizes.add(thumbnail_200, thumbnail_400)
+        except IntegrityError:
+            pass
